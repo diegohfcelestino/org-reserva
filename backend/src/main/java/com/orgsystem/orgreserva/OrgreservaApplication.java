ca59ae155fea7e55e6990a59004048d4202466d9
diegohfcelestino <diegohfcelestino@gmail.com>
2021-05-11 20:50:35 -0300

    Projeto criado

---
+++ backend/src/main/java/com/orgsystem/orgreserva/OrgreservaApplication.java
@@ -0,0 +1,13 @@
+package com.orgsystem.orgreserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgreservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgreservaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/orgsystem/orgreserva/OrgreservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orgsystem.orgreserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgreservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
