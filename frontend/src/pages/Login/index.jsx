5557466af2f140d0262f901fa8707d911c86f883
Adelmo Santos <adelmojsantos1985@gmail.com>
2021-06-30 21:00:03 -0300

    Reordenando scss

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -4,61 +4,63 @@ import Orgsystem from '../../assets/img/logo-org-tsplus.png'
 
 const Login = () => {
 
-    function logar() {
-        var email = document.getElementById("email");
-        var senha = document.getElementById("senha");
+  function logar() {
+    var email = document.getElementById("email");
+    var senha = document.getElementById("senha");
 
-        if (email.value === "admin@admin.com" && senha.value === "123") {
-            localStorage.setItem("acesso", true);
-            window.location.href = "/home";
-        } else {
-            alert("Usuário ou senha inválidos");
-        }
+    if (email.value === "admin@admin.com" && senha.value === "123") {
+      localStorage.setItem("acesso", true);
+      window.location.href = "/home";
+    } else {
+      alert("Usuário ou senha inválidos");
     }
+  }
 
-    return (
-        <>
-            {/* <NavBar showButton={false} /> */}
-            <div className="login">
-                <div className="header">
+  return (
+    <>
+      {/* <NavBar showButton={false} /> */}
+      <div className="login">
+        <div className="header">
+
+        </div>
+
+        <div className="container">
+          <div className="title">
+            <h2>
+              Agendamento de salas e veículos
+            </h2>
+          </div>
+          <div className="d-flex justify-content-center align-content-center mb-3">
+
+            <div className="row login-form" /* style={{ margin: 0, marginTop: 50, width: "40%" }} */>
+              <img src={Orgsystem} alt="Orgsystem" /* width="120" */ />
+              <form className="container">
+                <div className="mb-4">
+
+                  <label htmlFor="email" className="form-label">Email</label>
+                  <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                  <div id="emailHelp" className="form-text">Não compartilharemos seu e-mail com mais ninguém.</div>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="senha" className="form-label">Senha</label>
+                  <input type="password" className="form-control" id="senha" />
+                  <div id="emailHelp" className="form-text">Solicite a senha ao seu gestor.</div>
+                </div>
+                <div className="mb-3 form-check">
+                  <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                  <label className="form-check-label" htmlFor="exampleCheck1">Lembrar senha</label>
 
                 </div>
-
-                <div className="container">
-                    <div className="title">
-                        <h2 /* className="display-5 text-light" */>Agendamento de salas e veiculos</h2>
-                    </div>
-                    <div className="d-flex justify-content-center align-content-center mb-3">
-
-                        <div className="row login-form" /* style={{ margin: 0, marginTop: 50, width: "40%" }} */>
-                            <img src={Orgsystem} alt="Orgsystem" /* width="120" */ />
-                            <form className="container">
-                                <div className="mb-4">
-
-                                    <label htmlFor="email" className="form-label">Email</label>
-                                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
-                                    <div id="emailHelp" className="form-text">Não compartilharemos seu e-mail com mais ninguém.</div>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="senha" className="form-label">Senha</label>
-                                    <input type="password" className="form-control" id="senha" />
-                                    <div id="emailHelp" className="form-text">Solicite a senha ao seu gestor.</div>
-                                </div>
-                                <div className="mb-3 form-check">
-                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                    <label className="form-check-label" htmlFor="exampleCheck1">Lembrar senha</label>
-
-                                </div>
-                                <div className="d-grid gap-2">
-                                    <button onClick={() => logar()} type="button" className="btn btn-primary">Login</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+                <div className="d-grid gap-2">
+                  <button onClick={() => logar()} type="button" className="btn btn-primary">Login</button>
                 </div>
+              </form>
             </div>
-            <Footer />
-        </>
-    );
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 export default Login;
