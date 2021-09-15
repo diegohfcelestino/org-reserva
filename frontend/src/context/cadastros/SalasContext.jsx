e6a0875db70b3a3590294566b7404b757fcd7acc
Adelmo Santos <adelmojsantos1985@gmail.com>
2021-09-15 10:10:06 -0300

    Update Salas

--- frontend/src/context/cadastros/SalasContext.jsx
+++ frontend/src/context/cadastros/SalasContext.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-import { func } from "prop-types";
 import { createContext, useContext, useEffect, useState } from "react";
 import { supabase } from "../../supabaseClient";
 
@@ -9,12 +8,55 @@ export function SalaProvider({ children }) {
   const [salas, setSalas] = useState([])
 
   async function buscaSalas() {
-    let { data: salas, error } = await supabase
+    const { data: salas, error } = await supabase
       .from('salas')
       .select("*")
+      .order('id', { ascending: true })
 
-    //console.log(salas)
+    // console.log(salas)
     setSalas(salas)
+  }
+
+  async function insertSala(room) {
+    const { data, error } = await supabase
+      .from('salas')
+      .insert([
+        { name_sala: room }
+      ])
+
+    if (error) {
+      return alert(error)
+    } else {
+      buscaSalas()
+    }
+  }
+
+  async function deleteSala(id) {
+    const { data, error } = await supabase
+      .from('salas')
+      .delete()
+      .match({ id: id })
+
+    if (error) {
+      return alert(error)
+    } else {
+      buscaSalas()
+    }
+  }
+
+  async function updateSala(room) {
+    const { data, error } = await supabase
+      .from('salas')
+      .update(room)
+      .match({ id: room.id })
+
+    if (error) {
+      console.log(data)
+      return alert('Error updating room')
+    } else {
+      buscaSalas()
+      return data
+    }
   }
 
   useEffect(() => {
@@ -24,7 +66,10 @@ export function SalaProvider({ children }) {
   return (
     <SalaContext.Provider value={{
       salas,
-      buscaSalas
+      buscaSalas,
+      insertSala,
+      deleteSala,
+      updateSala
     }}>
       {children}
     </SalaContext.Provider>
